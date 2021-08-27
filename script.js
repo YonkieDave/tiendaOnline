ba6a4e8a8a3e824a7a63b9e5898854816ae648e0
YonkieDave <dtames1714@gmail.com>
2021-08-26 22:43:36 -0500

    Tienda online version que agrega articulos bootstrap

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-class SearchArticle {
-    constructor() {
-
-    }
-}
-
 class ShopingCart {
     constructor() {
         this.articles = [];
@@ -16,78 +10,95 @@ class ShopingCart {
     }
 }
 
-class articulo {
+class Articulo {
     constructor(data) {
         this.data = data;
     }
-    static async getArticle() {
-        console.log("obteniento url");
-        let url = `https://api.mercadolibre.com/sites/MLM/search?q=all`;
-        console.log(url);
-        const resp = await fetch(url);
-        const data = await resp.json();
-        let datosArticulo = new articulo(data);
-        console.log('Esta es la data del artículo --->', datosArticulo);
+    static async getArticle(articleSearch = '') {
+        if (articleSearch != '') {
 
-        await datosArticulo.CreateArticle();
+            console.log(`Entramos en la búsqueda ${articleSearch}`);
+            let url = `https://api.mercadolibre.com/sites/MLM/search?q=${articleSearch}`;
+            let resp = await fetch(url);
+            let data = await resp.json();
+            let datosArticulo = new Articulo(data);
+            await datosArticulo.CreateArticle();
+        } else {
+            console.log("obteniento url");
+            let url = `https://api.mercadolibre.com/sites/MLM/search?category=MLM1051`;
+            const resp = await fetch(url);
+            const data = await resp.json();
+            let datosArticulo = new Articulo(data);
+            await datosArticulo.CreateArticle();
+        }
     }
 
     async CreateArticle() {
 
 
         let datos = JSON.stringify(this.data.results);
-        console.log(JSON.parse(datos));
-        console.log('Aqui empieza el for');
 
+        let j = 0;
+        let newAddCart = [];
+
+        console.log(this.data);
 
         for (let i = 0; i < this.data.results.length; i++) {
 
-            let ctn = document.createElement('div');
-            ctn.id = "articulo-np";
-            let nombre = document.createElement('H2');
-            nombre.textContent = `${this.data.results[i].title} #${this.data.results[i].id}`;
-            nombre.id = `name`;
-            let img = document.createElement('img');
-            img.setAttribute('src', this.data.results[i].thumbnail);
-            let precio = document.createElement('H3');
-            precio.textContent = `$${this.data.results[i].price}`;
-            precio.id = `price`;
-            let boton = document.createElement("button", "Agregar-al-carrito");
-            boton.innerHTML = "Agregar al carrito";
-            boton.id = "Agregar-al-carrito";
-            boton.addEventListener("click", function addCart() {
-                console.log('Se va a agregar un articulo al carrito');
-                const nombreCarrito = document.getElementById('name');
-                const precioCarrito = document.getElementById('price');
-                console.log(nombreCarrito);
-                console.log(precioCarrito);
-                const carrito = new ShopingCart();
-                carrito.addArticle(nombreCarrito, precioCarrito);
+            const divProducts = document.createElement("div");
+            divProducts.setAttribute("id", "p" + i);
+            divProducts.setAttribute("class", "Card");
+            let producto = `
+            <div class = "card" style = "width: 18rem; margin-top: 20px">
+                <img src = ${this.data.results[i].thumbnail}/50px90/" alt = "Card image cap" > 
+                    <div class = "card-title form-control" " > 
+                        <h5 id = ${this.data.results[i].id} class = "card-title"> ${this.data.results[i].title}</h5>
+                        <h3 id=${j} >$${this.data.results[i].price}</h3 > 
+                        <a is= "5" name= "Add" class = "btn btn-primary" id = Boton ${j} onclick ="addCart();" > Agregar al carrito </a>
+                    </div>
+            </div>`;
+            divProducts.innerHTML += producto;
+            articleDiv.appendChild(divProducts);
+
+            newAddCart.push({
+                id: `${j}`,
+                name: `${this.data.results[i].title}`,
+                price: `${this.data.results[i].price}`
             });
-            ctn.appendChild(nombre);
-            ctn.appendChild(img);
-            ctn.appendChild(precio);
-            ctn.appendChild(boton);
-            articleDiv.appendChild(ctn);
+
+            j += 1;
 
         }
+
+        console.log(`Estos son los datos del articulo ----> ${JSON.stringify(newAddCart)}`);
 
     }
 }
 
 
+Articulo.getArticle();
 
-articulo.getArticle();
+
+const searchArticle = () => {
+    let articleSearch = document.getElementById("busqueda").value;
+    Articulo.getArticle(articleSearch);
+}
+
+function addCart() {
+    console.log(`valor del boton pulsado --- > `);
+    console.log('Se va a agregar un articulo al carrito');
+
+    //const carrito = new ShopingCart();
+    //carrito.addArticle(nombreCarrito, precioCarrito);
+}
+
+const getTendencias = async() => {
+    console.log("tendencia");
+    let url = `https://api.mercadolibre.com/trends/MLM/MLM1055`;
+    let resp = await fetch(url);
+    let data = await resp.json();
+
+    console.log(`Tendencia en México de celulares ${data}`);
 
 
-nameArticle = document.getElementById("busqueda").value;
-console.log(nameArticle);
-
-async function searchArticle() {
-    nameArticle = document.getElementById("busqueda").value;
-    console.log(nameArticle);
-    const url = `https://api.mercadolibre.com/sites/MLM/search?q=${nameArticle}`;
-    const resp = await fetch(url);
-    const data = await resp.json();
-    console.log(data);
 }
